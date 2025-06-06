e4664367f475dc55b24c1104cad6bdcde9237ecf
Mulawiak <oliwiermulawa@proton.me>
2025-06-06 13:26:19 +0200

    Commit innit

---
+++ src/test/java/Przyklad.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class Przyklad {
+
+    @Test
+    public void test()
+    {
+        int zmienna = 5;
+        int wynik = 3 + 2;
+        assert wynik == zmienna;
+    }
+}
